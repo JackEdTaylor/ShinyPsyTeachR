694a3efdaa1db681b68c12542e91a64dcdfef91f
Jack Taylor <jackt@psy.gla.ac.uk>
2019-06-27 15:30:08 +0100

    neaten

--- Probability/app.R
+++ Probability/app.R
@@ -1,31 +1,31 @@
 library(shiny)
 library(shinydashboard)
+library(shinyjs)
 library(tidyverse)
 library(magrittr)
-
-sidebarwidth <- 200
 
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
     skin = "blue",
     dashboardHeader(
-        title = "Probability", titleWidth = sidebarwidth,
+        title = "Probability", titleWidth = 200,
         tags$li(a(href = "https://github.com/JackEdTaylor/ShinyPsyTeachR",
                   HTML(paste(icon("github"), "&nbsp;GitHub")),
                   title = "GitHub Repository"), class="dropdown")
     ),
     
-    dashboardSidebar(
-        width = sidebarwidth,
-        numericInput("n_sim", "Number of Simulations", value = 100, min = 1, step =1, width = "100%"),
-        numericInput("n_flip", "Number of Flips Each Simulation", value = 10, min = 1, step = 1, width = "100%"),
-        sliderInput("p_heads", "Probability of Heads", value = 0.5, min = 0, max = 1, step = 0.05, width = "100%")
-    ),
+    dashboardSidebar(width = 0),
     
     dashboardBody(
+        useShinyjs(),  # enable shinyjs
         tags$style(type="text/css", ".recalculating {opacity: 1.0;}"),  # prevent plots from "greying out" while loading
         box(
             title = "Results", status = "primary", solidHeader = FALSE, width = 12, collapsible = FALSE,
+            fluidRow(
+                column(4, numericInput("n_sim", "Number of Simulations", value = 100, min = 1, step =1, width = "100%")),
+                column(4, numericInput("n_flip", "Number of Flips Each Simulation", value = 10, min = 1, step = 1, width = "100%")),
+                column(4, sliderInput("p_heads", "Probability of Heads", value = 0.5, min = 0, max = 1, step = 0.05, width = "100%"))
+            ),
             plotOutput("dist_plot"),
             uiOutput("animated_slider")
         )
@@ -34,6 +34,9 @@ ui <- dashboardPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    
+    # hide sidebar
+    addClass(selector = "body", class = "sidebar-collapse")
     
     flips <- reactive({
         n_sim <- input$n_sim
@@ -60,34 +63,43 @@ server <- function(input, output) {
     })
     
     output$dist_plot <- renderPlot({
-       if (is.null(input$i_sim)) {
-           NULL
-       } else {
-           n_flip <- input$n_flip
-           this_sim <- input$i_sim
-           
-           max_obs <- flips() %>%
-               group_by(n_heads) %>%
-               summarise(obs = n()) %>%
-               pull(obs) %>%
-               max()
-           
-           flips() %>%
-               filter(sim_nr <= this_sim) %>%
-               mutate(is_this_sim = factor(ifelse(sim_nr == this_sim, "This Simulation", "Previous Simulations"), levels = c("This Simulation", "Previous Simulations"))) %>%
-               ggplot(aes(x = n_heads, fill = is_this_sim)) +
-               geom_bar() +
-               labs(
-                   x = sprintf("Number of Heads out of %i Flips", n_flip),
-                   y = "Number of Simulations Observed"
-               ) +
-               theme_minimal() +
-               theme(legend.position = "top", legend.title = element_blank()) +
-               scale_x_continuous(limits = c(0, n_flip), breaks = scales::pretty_breaks()) +
-               scale_y_continuous(limits = c(0, max_obs)) +
-               scale_fill_manual(values=c("#3c8dbc", "#999999"))
-       }
-    })
+        if (is.null(input$i_sim)) {
+            NULL
+        } else {
+            n_flip <- input$n_flip
+            n_sim <- input$n_sim
+            this_sim <- input$i_sim
+            
+            max_obs <- flips() %>%
+                group_by(n_heads) %>%
+                summarise(obs = n()) %>%
+                pull(obs) %>%
+                max()
+            
+            flips() %>%
+                filter(sim_nr <= this_sim) %>%
+                mutate(is_this_sim = factor(ifelse(sim_nr == this_sim, "This Simulation", "Previous Simulations"), levels = c("This Simulation", "Previous Simulations"))) %>%
+                ggplot(aes(x = n_heads, fill = is_this_sim)) +
+                geom_bar() +
+                labs(
+                    x = sprintf("\nNumber of Heads out of %i Flips", n_flip),
+                    y = "Number of Simulations Observed\n",
+                    caption = sprintf("\nSimulation %i/%i", this_sim, n_sim)
+                ) +
+                theme_minimal() +
+                theme(
+                    legend.text = element_text(size = 15),
+                    legend.position = "top",
+                    legend.title = element_blank(),
+                    axis.title = element_text(size = 14),
+                    axis.text = element_text(size = 12),
+                    plot.caption = element_text(size = 13)
+                ) +
+                scale_x_continuous(limits = c(0, n_flip), breaks = scales::pretty_breaks()) +
+                scale_y_continuous(limits = c(0, max_obs)) +
+                scale_fill_manual(values=c("#3c8dbc", "#999999"))
+        }
+    }, execOnResize = TRUE)
     
 }
 
